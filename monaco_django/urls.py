652a9066c3abb9cc20296bcefec7958c2bc295cc
AlexNdungu <alexmeta517@gmail.com>
2024-04-06 16:01:20 +0300

    install monaco package

--- monaco_django/urls.py
+++ monaco_django/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('editor.urls')),
+
+    re_path(r'^vs/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT + '/vs',
+    }),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
